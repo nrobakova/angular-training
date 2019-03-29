584262042a912c0ffc57e9c4566a52e0b4b5ccac
Nadia Robakova <nrobakova@infragistics.com>
2019-03-29 12:47:40 +0200

    test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  title = 'Welcome to Ignite UI for Angular!';
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
